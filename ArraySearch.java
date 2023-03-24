b42c8e97055858e8b79981e438dc0d916de47741
A-Tina-Davis <22c007@gectcr.ac.in>
2023-03-24 12:58:47 +0530

    first commit

--- ArraySearch.java
+++ ArraySearch.java
@@ -2,20 +2,28 @@ import java.util.Scanner;
 public class ArraySearch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int flag=0,i;
 		int ar[] = new int[20];
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the size of the array:");
 		int n = scan.nextInt();
 		System.out.println("Enter the array of integers:");
-		for(int i=0;i<n;i++) {
+		for(i=0;i<n;i++) {
 			ar[i] = scan.nextInt();
 		}
 		System.out.print("Enter an element to search:");
 		int s = scan.nextInt();
-		System.out.println("Element found at location: ");
-		for(int i=0;i<n;i++) {
-			if(s==ar[i])
-				System.out.print(i+1+" ");
+		for(i=0;i<n;i++) {
+			if(s==ar[i]) {
+				flag=1;
+				break;
+			}
+		}
+		if(flag==1) {
+			System.out.println("Element found at location: "+(i+1));
+		}
+		else {
+			System.out.println("Element not found!!! ");
 		}
 	}
 
